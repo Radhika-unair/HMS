1d55dd1e0ff7c0dfcd47403dcf3870317c954820
Arunjith <arunjithk987@Gmail.com>
2025-03-12 00:44:28 +0530

    assets-connected-to-API

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target:
+          "https://0b1c-2409-40f3-1015-99e9-7d54-f406-4707-6649.ngrok-free.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
+      },
+    },
+  },
+});
