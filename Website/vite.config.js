3fc11384794e35be257acf8f30a39e7eaf292e59
Arunjith <arunjithk987@Gmail.com>
2025-03-13 09:46:44 +0530

    assets

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -6,13 +6,29 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/asset": {
         target:
           "https://0b1c-2409-40f3-1015-99e9-7d54-f406-4707-6649.ngrok-free.app",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
       },
     },
+    cors: true,
   },
 });
